52aa5fd301d86740ec0a06216da5595f977c6e35
Joe McConnell <joe.mcconnell10@gmail.com>
2022-06-03 23:11:55 -0500

    it works as a sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [users , setUsers] = useState([]);
+  useEffect(()=>{
+    const fetchData = async() => {
+      const response = await axios.get("https://20gefk5dd7.execute-api.us-east-1.amazonaws.com/dev/query");
+      setUsers(response.data)
+    }
+    fetchData();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>List of Users</h1>
+      {users.map(item=>(
+        <ul>
+          <li>{item.name}</li>
+          <li>{item.activity}</li>
+          </ul>
+      ))}
     </div>
   );
 }
